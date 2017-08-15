aef16836df8cfe79af21dc5568f86a769b4efc74
MagikStrong <cybercentiped@protonmail.com>
2017-08-15 09:25:32 +0200

    index

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 include('config/autoload.php');
 
-if(isset($_POST['inscription']) AND isset($_POST['username']) AND isset($_POST['password'])){
-	register($db, $_POST['username'], $_POST['password']);
-}
 
 if(isset($_POST['connexion']) AND isset($_POST['username']) AND isset($_POST['password'])){
 	login($db, $_POST['username'], $_POST['password']);
@@ -32,18 +29,14 @@ if(isset($_SESSION['id'])){
 <div class="col-lg-6">
 	<div class="well bs-components">
 
-<h2>Inscription</h2>
-<form action="index.php" method="post">
-	<input type="text" name="username" placeholder="Nom d'utilisateur">
-	<input type="password" name="password" placeholder="Mot de passe">
-	<input type="submit" name="inscription" value="Connexion">
-</form>
 
 <h2>Connexion</h2>
 <form action="index.php" method="post">
 	<input type="text" name="username" placeholder="Nom d'utilisateur">
 	<input type="password" name="password" placeholder="Mot de passe">
 	<input type="submit" name="connexion" value="Connexion">
+	
+<p class="account">Vous n'avez pas de compte ? <a href="register.php">Inscrivez vous !</a></p>
 </form>
 							</div>
 						</div>
